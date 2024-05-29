449dc2ae7011d7b5089f468cdd49065461ed7b50
devteam0099 <av.dev2024@gmail.com>
2024-05-29 17:58:13 +0500

    added index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import Smartslider from './components/slider';
+//import Expensehandler from './components/expense';
+//import Tempconverter from './components/tempconverter.js';
+//import SaveButton from './webfrom';
+//import Passwordgenerator from './components/password';
+import './index.css';
+//import App from './components/expense.js';
+import Webform from './webfrom';
+//import Clock from './components/clock';
+import Prc from './components/practice';
+//import Stopwatch from './components/stopwatch';
+//import Prc from './components/practice.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+   
+  
+   <Prc />
+  </React.StrictMode>
+);
+
